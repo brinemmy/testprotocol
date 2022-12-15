ae6fe55907f96047fb413d321aae4d5385ad8694
brinemmy <120670159+brinemmy@users.noreply.github.com>
2022-12-15 18:38:04 +0100

    Add files via upload

---
+++ wallets/Package/PHPMailer/export.php
@@ -0,0 +1,8 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
